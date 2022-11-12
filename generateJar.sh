44da07511072ebb6c66c44657ff949144715d7c0
Maya Coles <mayacoles02@gmail.com>
2022-11-12 10:41:39 -0500

    Restructured project to add build command generateJar.sh

---
+++ generateJar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mvn clean;
+mvn package;
+java -jar target/Polar-Bear-Game.jar
--- src/main/java/incrementalgame/IncrementalGame.java
+++ src/main/java/incrementalgame/IncrementalGame.java
@@ -1,10 +1,25 @@
 package incrementalgame;
 import Utils.Globals;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.*;
+import GameObjects.*;
 
 public class IncrementalGame {
 
     public static void main(String[] args) {
-         Initialize.initializeSaveFileChooser();
+        Initialize.initializeSaveFileChooser();
+        /*Initialize.initializeConfig();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try {
+            Writer f = new FileWriter("upgrades.json");
+            gson.toJson(Globals.ALL_UPGRADES, f);
+
+            f.flush();
+            f.close();
+        }
+        catch(Exception e){}*/
+
     }
     public static void testing(){
         for(int i = 0; i < Globals.NUMBER_OF_RESOURCES; i++){
